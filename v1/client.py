373a33e637bdea3f026db956154a580e47cb1174
TheoManea <theomanea9@gmail.com>
2023-12-19 17:28:04 +0100

    client up, channels created, handling messages

--- v1/client.py
+++ v1/client.py
@@ -1,26 +1,35 @@
 import socket
 import threading
+import sys
 
+listening = True
 
 def listen(socket):
     while True:
         data = socket.recv(1024).decode()
-        print('Received from server: ' + str(data))
-
+        if not data: 
+            listening = False
+            return
+        print(str(data))
 
 if __name__ == "__main__":
 
-    client_socket = socket.socket() 
-    client_socket.connect((socket.gethostname(), 5500)) 
+    if len(sys.argv) < 2:
+        print("usage : client.py <nickname>")
+    else:
+        client_socket = socket.socket() 
+        client_socket.connect((socket.gethostname(), 5500)) 
 
-    listen_thread = threading.Thread(target=listen,args=(client_socket,))
-    listen_thread.start()
+        listen_thread = threading.Thread(target=listen,args=(client_socket,))
+        listen_thread.start()
 
-    message = input(" -> ") 
+        #setting up the params
 
-    while message.lower().strip() != 'exit':
-        client_socket.send(message.encode()) 
+        message = input("") 
 
-        message = input(" -> ")
+        while message.lower().strip() != 'exit' and listening :
+            client_socket.send(message.encode()) 
+            message = input("")
+            
 
-    client_socket.close() 
+        client_socket.close() 
--- v1/server.py
+++ v1/server.py
@@ -18,6 +18,22 @@ class User:
 	def get_nickname(self):
 		return self.nickname
 
+class Channel:
+	def __init__(self,name_channel):
+		self.name_channel = name_channel
+		self.user_list = []
+
+	def get_user_list(self):
+		return self.user_list
+
+	def add_user(self,user):
+		print(f"  >>>> {user.get_nickname()} has been added to the {self.name_channel} channel")
+		self.user_list.append(user)
+
+	def send_channel(self,data):
+		for user in self.user_list:
+			user.get_conn().send(data.encode())
+
 class Server:
 	def __init__(self, server_name):
 		self.server_name = server_name
@@ -25,20 +41,71 @@ class Server:
 		self.port = 5500
 		self.socket_server = None
 		self.list_current_user = []
+		self.channel_list = {}
 
 	def print_current_users(self):
 		self.prRed(f" >>>> [LOG] There are {len(self.list_current_user)} connected users")
 		threading.Timer(30, self.print_current_users).start()
 
+	def send_all(self,user,data,channel="default"):
+		(self.channel_list[channel]).send_channel(f"{user.get_nickname()} : {data}") 
+
+	def send_private(self,user,data):
+		user.get_conn().send(data.encode())
+
+	def parse_command(self,user,data):
+		cmd = data.split(" ")
+		
+		if len(cmd) == 0:
+			self.send_private(user,"please provide a correct command")
+			return
+
+		match cmd[0]:
+			case "/away":
+				pass
+			case "/help":
+				pass
+			case "/invite":
+				pass
+			case "/join":
+				pass
+			case "/list":
+				pass
+			case "/msg":
+				if len(cmd) == 1:
+					self.send_private(user,"Incorrect syntax")
+				else:
+					if (cmd[1])[0] == '[' and (cmd[1])[len(cmd[1])-1] == ']': #specifying a canal
+					   	canal_name = cmd[1:-1]
+					   	self.send_all(user,"TO-DO",canal_name)
+					else: #messaging in the default channel 
+						msg = " ".join(cmd[1:])
+						self.send_all(user,msg,"default")
+
+			case "/names":
+				pass
+			case _:
+				self.send_private(user,"This command does not exists...") 
+
+	def create_channel(self,name):
+		self.channel_list[name] = Channel(name)
+		self.prGreen(f" >>>> The {name} channel has been created")
+
 	def handle_user(self,user):
-		print(" >>>> New user")
+		print(f"  >>>> New user {user.get_nickname()}")
+
+		#by default we add all users to the default channel
+		(self.channel_list["default"]).add_user(user)
+
+		self.send_private(user,f" >>>> You succesfully connected to {self.server_name}")
 
 		while True:
 			data = user.get_conn().recv(1024).decode()
 			if not data:
-				print(f"{user.get_addr()} has left")
+				print(f"  >>>> {user.get_addr()} has left")
 				break
-			print(f"{user.get_addr()} : {str(data)}")
+			self.parse_command(user,data)
+			
 
 	def start_server(self):
 		self.socket_server = socket.socket()
@@ -46,16 +113,20 @@ class Server:
 		self.socket_server.bind((socket.gethostname(),5500))
 		self.prGreen('''
 
-			  _			    ___				    ___  _   _ 
+			  _				___					___  _   _ 
 			 /   _   _  _ __ |  _ |_   _. _|_ __ |  |_) /  
 			 \_ (_) (_ (_)   | (_ | | (_|  |_   _|_ | \ \_ 
 											   
 
 			''')
 		self.prGreen(" >>>> Server Started ")
-		self.socket_server.listen(5)
 
+		#create default channel
+		self.create_channel("default")
+
+		self.socket_server.listen(5)
 		self.print_current_users()
+
 
 		while True:
 			#receiving conn params
